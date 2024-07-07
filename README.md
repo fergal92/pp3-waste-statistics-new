2b9765affe8c7ae04ef4792d3e7a4392d5b80d6a
Fergal Mulligan <fshaq4@gmail.com>
2024-07-07 15:37:03 +0000

    Create function to update tonneage amount in collector worksheets

--- README.md
+++ README.md
@@ -7,37 +7,46 @@ Access the program here - [Waste Data Analyser](https://waste-data-analyser-3742
 ![Responsive Image]()
 ## User stories
 
-### As a user I can...
+### As a user I can use the  program to enter valid data into a spreadsheet from the command line
 #### Acceptance Criteria:
-
+- Enter data via the CLI
+- Ensure the data is validated appropriately
+- The data is saved into the spreadsheet
 #### Tasks:
-- Build an easy to use navbar
-- Have the nav bar stick to the top of the page at all times
+- Create an input option for the program
+- Create validation parameters for the data 
+- Link the sheet via an API to the program to save all data entered 
 
-### As a user I can...
+### As a user I can use the  program to analyse the data and compare waste collectors for each month and quarter
 #### Acceptance Criteria:
-
+- Allow the user to compare total collected waste for each month and quarter
 #### Tasks:
+- Create functions that allow comparison of data between sheets
+
+### As a user I can use the  program to analyse how much profit the waste collecors made 
+#### Acceptance Criteria:
+- Allow the user to see how much gross and net profit was made
+#### Tasks:
+- Create functions that calculate the tonnes collected with the price list
+- subtract the net profit from the gross profit
 
 ## Agile Methodologies
 ### GitHub Projects
-Link to the GitHub project page that was used to manage the website build. Items were added to the kanban board and worked through. Items were linked to the project repo and closed off as the project progressed. [github project board]()
+Link to the GitHub project page that was used to manage the website build. Items were added to the kanban board and worked through. Items were linked to the project repo and closed off as the project progressed. [github project board](https://github.com/users/fergal92/projects/3/views/1)
+
 ## Features
 ### Existing Features
 
 ### Features left to implement 
 
 ## Technologies Used
-- [VSCode](https://code.visualstudio.com/) - was used as the main tool to write and edit code.
+- [GitPod](https://www.gitpod.io/) - was used as the main tool to write and edit code.
 - [Git](https://git-scm.com/) - was used for the version control of the website.
 - [GitHub](https://github.com/) - was used to host the code of the website.
+- [Python](https://www.python.org/)
 - [Am I Responsive](https://ui.dev/amiresponsive) - was used to generate an image of the website across different screen types and resolutions
 
 ## Design
-### Colours
-
-## Typography
-
 ### Wireframes
 #### Mobile Wireframes
 
@@ -48,7 +57,7 @@ Please refer to the [TESTING.md](TESTING.md) file for all testing documentation
 ## Deployment
 
 ### Deployment to Heroku
-
+The heroku app was deployed at the begonning of the project to ensure any issues were encountered early and could be resolved.
 
 The live link to the site can be found here https://waste-data-analyser-3742936dc8c7.herokuapp.com/
 
--- run.py
+++ run.py
@@ -1,19 +1,117 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
+from pprint import pprint
 
+# Scope definition
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
+# Get API google sheet working
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("waste_data")
 
-facility_A = SHEET.worksheet("facility-A")
+# Function to select worksheet
+def select_worksheet():
+    """
+    Function that selects a collector worksheet
+    """
+    while True:
+        worksheet_name = input("Enter the worksheet name to update (collector-a, collector-b, or collector-c): ").strip().lower()
+        if worksheet_name in ["collector-a", "collector-b", "collector-c"]:
+            return worksheet_name
+        else:
+            print("Invalid worksheet name. Please enter collector-a, collector-b, or collector-c.\n")
 
-data = facility_A.get_all_values()
+def get_monthly_waste_data():
+    """
+    Get monthly waste figures input from user
+    """
 
-print(data)
+    while True:
+
+        print("Please enter your waste data from the last month")
+        print("Data should be 4 numbers separated by commas")
+        print("Example: 180,80,60,40")
+        print("The 1st number is C & D waste,")
+        print("The 2nd number is Black bin waste,")
+        print("The 3rd number is Green bin waste,")
+        print("The 4th number is Brown bin waste\n")
+
+        data_str = input("Enter your data here: ")
+
+        data = data_str.split(",")
+        
+
+        if validate_data(data):
+            print('Data is valid')
+            break
+
+    return data
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values to integers. Raises ValueError if string cannot be converted into int, or if there aren't exatly 4 values
+    """
+
+    try:
+        # Convert each value to integer
+        [int(value) for value in values]
+        # Check if exactly 4 values are provided
+        if len(values) !=4:
+            raise ValueError(
+                f"Exactly 4 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+def update_worksheet(data, worksheet_name):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data to be provided
+    Data should be entered into column H
+    """
+    print(f"Updating {worksheet_name} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet_name)
+    next_empty_row = find_next_empty_row(worksheet_to_update)
+
+    # Ensure we are updating column H only
+
+    # Ensure we are updating column H only
+    for i, value in enumerate(data):
+        worksheet_to_update.update_cell(next_empty_row + i, 8, int(value))  # Column H is 8 in 1-based index
+
+    print(f"{worksheet_name} worksheet updated successfully\n")
+
+def find_next_empty_row(worksheet):
+    """
+    Function to find the next empty cell in column H
+    """
+    col_h = worksheet.col_values(8)  # Column H is index 8 (1-based index)
+    next_empty_index = len(col_h) + 1  # Index of the next empty cell
+    return next_empty_index
+
+def main():
+    """
+    Main function to run the program.
+    """
+    print('Welcome to Waste Data Analyzer')
+
+    # Select worksheet
+    worksheet_name = select_worksheet()
+
+    # Get monthly waste data
+    data = get_monthly_waste_data()
+    waste_data = [int(num) for num in data]
+
+    # Update the selected worksheet with the waste data
+    update_worksheet(waste_data, worksheet_name)
+
+main()
